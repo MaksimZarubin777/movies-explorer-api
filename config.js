4306545371b3907cd2d2ec62e98466c0eaf0e0ac
Maksim Zarubin <89940028777@ya.ru>
2023-06-04 20:57:06 +0400

    fix:mongoUrl

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ const config = {
   env: process.env.NODE_ENV || 'development',
   jwtSecret: process.env.JWT_SECRET || 'dev-secret',
   port: process.env.PORT || 3000,
-  mongoDbUrl: process.env.MONGODB_URL || 'mongodb://127.0.0.1:27017/bitfilmsdb',
+  mongoDbUrl: process.env.MONGODB_URL || 'mongodb://158.160.51.43:27017/bitfilmsdb',
 };
 
 module.exports = config;
