801b124ae0dcb1efb41ed0a3d10dce07a25a2995
Maksim Zarubin <89940028777@ya.ru>
2023-07-23 04:17:13 +0400

    dip_back_cors_options_added-orogin

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -14,10 +14,11 @@ function handleCors(req, res, next) {
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
   const requestHeaders = req.headers['access-control-request-headers'];
   if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', 'http://movexp.nomoredomains.rocks');
+    res.header('Access-Control-Allow-Origin', origin);
     res.header('Access-Control-Allow-Credentials', 'true');
   }
   if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Origin', origin);
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
     return res.end();
